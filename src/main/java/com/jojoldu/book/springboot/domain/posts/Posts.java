8c844e76f94636cfd19f32f42561cc918af34076
Sooya0441 <github.hlee1@gmail.com>
2023-02-07 16:28:08 +0900

    2023-02-07

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.domain.posts;
 
+import com.jojoldu.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,7 +26,7 @@ import javax.persistence.Id;
  * 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍(_)으로 테이블 이름을 매칭합니다.
  * ex) SalesManager.java -> sales_manager table
  */
-class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     /** @Id
      * 해당 테이블의 PK 필드를 나타냅니다.
@@ -33,7 +34,7 @@ class Posts {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     /** @GeneratedValue
      * PK 의 생성 규칙을 나타냅니다.
-     * 스프링 부트 2.0에서는 GenerationType.IDENTITY 옵션을 추가해야만 auto_increment가 됩니다.
+     * 스프링 부트 2.0에서는 GenerationType.IDENTITY 옵션을 추가해야만 auto_increment 가 됩니다.
      * 스프링 부트 2.0 버전과 1.5 버전의 차이는 https://jojoldu.tistory.com/295 에 정리했으니 참고하세요.
      */
     private Long id;
@@ -42,7 +43,7 @@ class Posts {
     /** @Column
      * 테이블의 칼럼을 나타내며 굳이 선언하지 않더라도 해당 클래스의 필드는 모두 칼럼이 됩니다.
      * 사용하는 이유는, 기본값 외에 추가로 변경이 필요한 옵션이 있으면 사용합니다.
-     * 문자열의 경우 VARCHAR(255)가 기본값인데, 사이즈를 500으로 늘리고 싶거나(ex: title), 타입을 TEXT로 변경하고 싶거나(ex: content) 등의 경우에 사용됩니다.
+     * 문자열의 경우 VARCHAR(255)가 기본값인데, 사이즈를 500으로 늘리고 싶거나(ex: title), 타입을 TEXT 로 변경하고 싶거나(ex: content) 등의 경우에 사용됩니다.
      */
     private String title;
 
@@ -60,5 +61,10 @@ class Posts {
         this.title = title;
         this.content = content;
         this.author = author;
+    }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
     }
 }
