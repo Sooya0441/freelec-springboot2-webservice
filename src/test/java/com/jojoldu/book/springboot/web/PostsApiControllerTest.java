8d8bfcb9eb7ebd0e8fa905c9e83ff52ac071714b
Sooya0441 <github.hlee1@gmail.com>
2023-02-17 14:35:04 +0900

    2023-02-1 5.7 기존 테스트에 시큐리티 적용 - 문제2. 302 Status Code

--- src/test/java/com/jojoldu/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/PostsApiControllerTest.java
@@ -1,10 +1,12 @@
 package com.jojoldu.book.springboot.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,19 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+/** import...
+ * 모두 새로 추가되는 부분입니다.
+ * 나머지는 기존과 동일합니다.
+ */
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -32,12 +47,33 @@ public class PostsApiControllerTest {
     @Autowired
     private PostsRepository postsRepository;
 
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mvc;
+
+    @Before
+    /** @Before
+     * 매번 테스트가 시작되기 전에 MockMvc 인스턴스를 생성합니다.
+     */
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
     @After
     public void tearDown() throws Exception {
         postsRepository.deleteAll();
     }
 
     @Test
+    @WithMockUser(roles = "USER")
+    /**  @WithMockUser(roles = "USER")
+     * 인증된 모의(가짜) 사용자를 만들어서 사용합니다.
+     * roles 에 권한을 추가할 수 있습니다.
+     * 즉, 이 어노테이션으로 인해 ROLE_USER 권한을 가진 사용자가 API 를 요청하는 것과 동일한 효과를 가지게 됩니다.
+     */
     public void Posts_등록된다() throws Exception {
         //given
         String title = "title";
@@ -51,18 +87,30 @@ public class PostsApiControllerTest {
         String url = "http://localhost:" + port + "/api/v1/posts";
 
         //when
-        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+        /** MockMvcBuilders 사용에 따른 삭제된 코드
+         * \\ ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+         */
+        mvc.perform(post(url)
+                /** mvc.perform
+                 * 생성된 MockMvc 를 통해 API 를 테스트합니다.
+                 * 본문(body) 영역은 문자열로 표현하기 위해 ObjectMapper 를 통해 문자열 JSON 으로 변환합니다.
+                 */
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
 
         //then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
-
+        /** MockMvcBuilders 사용에 따른 삭제된 코드
+         * \\ assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+         * \\ assertThat(responseEntity.getBody()).isGreaterThan(0L);
+         */
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void Posts_수정된다() throws Exception{
         //given
         Posts savedPosts = postsRepository.save(Posts.builder()
@@ -84,12 +132,19 @@ public class PostsApiControllerTest {
         HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
 
         //when
-        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+        /** MockMvcBuilders 사용에 따른 삭제된 코드
+         * \\ ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+         */
+        mvc.perform(put(url)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
 
         //then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
-
+        /** MockMvcBuilders 사용에 따른 삭제된 코드
+         * \\ assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+         * \\ assertThat(responseEntity.getBody()).isGreaterThan(0L);
+         */
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
         assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
